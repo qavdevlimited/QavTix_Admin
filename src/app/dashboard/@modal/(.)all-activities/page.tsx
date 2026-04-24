7909fde39aecd5632c4fc215f80d26cb2b4e8e40
Nelson Onuegbu <leonenelson38@gmail.com>
2026-04-24 23:24:10 +0100

    Init commit to repo

--- src/app/dashboard/@modal/(.)all-activities/page.tsx
+++ src/app/dashboard/@modal/(.)all-activities/page.tsx
@@ -1,6 +1,7 @@
 import { getAdminActivities } from "@/actions/dashboard"
 import AllActivitiesModal from "@/components/modals/AllActivitiesModal"
 
+
 export default async function AllActivitiesModalPage() {
     const res = await getAdminActivities(1)
     return <AllActivitiesModal initialData={res.data} />
